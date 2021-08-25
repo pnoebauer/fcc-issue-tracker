eba0505f978650ea0b4f715b9dc4aac4a168e806
Philip Noebauer <pnoebauer>
2021-08-25 19:38:50 +0800

    added further test cases

--- routes/api.js
+++ routes/api.js
@@ -36,20 +36,24 @@ module.exports = async function (app) {
 		.route('/api/issues/:project')
 
 		.get(async function (req, res) {
-			let project = req.params.project;
+			const project = req.params.project;
 
 			// return everything except the __v field
-			const issues = await Issue.find({}).select('-__v').exec();
+			// const issues = await Issue.find({}).select('-__v').exec();
+			const issues = await Issue.find({project_title: project, ...req.query})
+				.select('-__v')
+				.exec();
 
-			console.log(issues);
-			// const issues = await Issue.find({project_title: project}).select('-__v').exec();
+			// console.log(req.query);
+
+			return res.json(issues);
 		})
 
 		.post(async function (req, res) {
 			const project = req.params.project;
 
-			console.log(req.body, req.params);
-			let issueObject = {project_title: project};
+			// console.log(req.body, req.params);
+			let issueObject = {project_title: project, open: true};
 
 			Object.keys(req.body).forEach(
 				key => req.body[key].length && (issueObject[key] = req.body[key])
@@ -59,6 +63,8 @@ module.exports = async function (app) {
 				const addedIssue = await new Issue(issueObject).save();
 
 				const {
+					_id,
+					project_title,
 					issue_title,
 					issue_text,
 					created_by,
@@ -72,6 +78,8 @@ module.exports = async function (app) {
 				if (addedIssue) {
 					// return res.json(addedIssue);
 					return res.json({
+						_id,
+						project_title,
 						issue_title,
 						issue_text,
 						created_by,
@@ -81,10 +89,12 @@ module.exports = async function (app) {
 						updated_at: updated_at.toDateString(),
 					});
 				} else {
-					return res.json({error: 'error adding the issue to the db'});
+					return res.status(400).json({error: 'error adding the issue to the db'});
 				}
 			} catch (e) {
-				return res.json({error: 'error adding issue, check the form input data types'});
+				return res
+					.status(400)
+					.json({error: 'error adding issue, check the form input data types'});
 			}
 		})
 
@@ -100,19 +110,53 @@ module.exports = async function (app) {
 				key => updatedData[key].length && (updatedIssueObject[key] = updatedData[key])
 			);
 
-			console.log(req.body);
+			// console.log(req.body);
 
 			// const findIssue = await Issue.findById(_id).exec();
 			// console.log(findIssue);
+			try {
+				const updatedIssue = await Issue.findOneAndUpdate({_id}, updatedIssueObject, {
+					// upsert: false,
+					new: true,
+				}).select('-__v');
+				// console.log(updatedIssue, '------up');
 
-			const updatedIssue = await Issue.findOneAndUpdate({_id}, updatedIssueObject, {
-				// upsert: false,
-				new: true,
-			}).select('-__v');
-			console.log(updatedIssue, '------up');
+				const {
+					issue_title,
+					issue_text,
+					created_by,
+					assigned_to,
+					status_text,
+					created_at,
+					updated_at,
+				} = updatedIssue;
+
+				if (updatedIssue) {
+					// return res.json(addedIssue);
+					return res.json({
+						issue_title,
+						issue_text,
+						created_by,
+						assigned_to,
+						status_text,
+						created_at: created_at.toDateString(),
+						updated_at: updated_at.toDateString(),
+					});
+				} else {
+					return res.status(400).json({error: 'error updating the issue in the db'});
+				}
+			} catch (e) {
+				return res
+					.status(400)
+					.json({error: 'error updating issue, check the form input data types'});
+			}
 		})
 
 		.delete(async function (req, res) {
 			const project = req.params.project;
+
+			const {_id} = req.body;
+			const deletedIssue = await Issue.findByIdAndDelete({_id}).select('-__v');
+			// console.log(deletedIssue, '------deleted');
 		});
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,186 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite('Functional Tests', function () {
+	let createdId = '';
+	test('Create an issue with every field: POST request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.post('/api/issues/apitest')
+			.send({
+				issue_title: 'test',
+				issue_text: 'test case',
+				created_by: 'Phil',
+				assigned_to: 'Joe',
+				status_text: 'Not finished',
+			})
+			.end(function (err, res) {
+				createdId = res.body._id;
+
+				assert.equal(res.status, 200);
+				// assert.equal(res.body.issue_title, 'test');
+				// assert.equal(res.body.issue_text, 'test case');
+				assert.deepEqual(res.body, {
+					_id: res.body._id,
+					project_title: 'apitest',
+					issue_title: 'test',
+					issue_text: 'test case',
+					created_by: 'Phil',
+					assigned_to: 'Joe',
+					status_text: 'Not finished',
+					created_at: new Date().toDateString(),
+					updated_at: new Date().toDateString(),
+				});
+				done();
+			});
+	});
+	test('Create an issue with only required fields: POST request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.post('/api/issues/apitest')
+			.send({
+				issue_title: 'test 2',
+				issue_text: 'test case 2',
+				created_by: 'Phil',
+			})
+			.end(function (err, res) {
+				assert.equal(res.status, 200);
+				// assert.equal(res.body.issue_title, 'test');
+				// assert.equal(res.body.issue_text, 'test case');
+				assert.deepEqual(res.body, {
+					_id: res.body._id,
+					project_title: 'apitest',
+					issue_title: 'test 2',
+					issue_text: 'test case 2',
+					created_by: 'Phil',
+					created_at: new Date().toDateString(),
+					updated_at: new Date().toDateString(),
+				});
+				done();
+			});
+	});
+	test('Create an issue with missing required fields: POST request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.post('/api/issues/apitest')
+			.send({
+				issue_title: 'test 2',
+				issue_text: 'test case 2',
+			})
+			.end(function (err, res) {
+				assert.equal(
+					res.body.error,
+					'error adding issue, check the form input data types'
+				);
+				assert.equal(res.status, 400);
+				done();
+			});
+	});
+	test('View issues on a project: GET request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.get('/api/issues/apitest')
+			.end(function (err, res) {
+				assert.isArray(res.body);
+				done();
+			});
+	});
+	test('View issues on a project with one filter: GET request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.get('/api/issues/apitest?open=true')
+			.end(function (err, res) {
+				assert.isArray(res.body);
+
+				const allOpenTrue = res.body.every(issue => issue.open);
+				assert.isTrue(allOpenTrue);
+
+				res.body.forEach(issue => assert.isTrue(issue.open, issue.issue_title));
+
+				done();
+			});
+	});
+	test('View issues on a project with multiple filters: GET request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.get('/api/issues/apitest?open=true&assigned_to=Joe')
+			.end(function (err, res) {
+				assert.isArray(res.body);
+
+				const allOpenTrue = res.body.every(
+					issue => issue.open && issue.assigned_to === 'Joe'
+				);
+				assert.isTrue(allOpenTrue);
+
+				res.body.forEach(issue =>
+					assert.isTrue(issue.open && issue.assigned_to === 'Joe', issue.issue_title)
+				);
+
+				done();
+			});
+	});
+	test('Update one field on an issue: PUT request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.put('/api/issues/apitest')
+			.send({
+				_id: createdId,
+				issue_title: 'updated test',
+			})
+			.end(function (err, res) {
+				assert.equal(res.status, 200);
+				assert.equal(res.body.issue_title, 'updated test');
+				// assert.deepEqual(res.body, {
+				// 	issue_title: 'updated test',
+				// 	issue_text: 'test case',
+				// 	created_by: 'Phil',
+				// 	assigned_to: 'Joe',
+				// 	status_text: 'Not finished',
+				// 	created_at: new Date().toDateString(),
+				// 	updated_at: new Date().toDateString(),
+				// });
+				done();
+			});
+	});
+	test('Update multiple fields on an issue: PUT request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.put('/api/issues/apitest')
+			.send({
+				_id: createdId,
+				issue_title: 'updated test',
+				issue_text: 'updated test case',
+			})
+			.end(function (err, res) {
+				assert.equal(res.status, 200);
+				assert.equal(res.body.issue_title, 'updated test');
+				assert.equal(res.body.issue_text, 'updated test case');
+				// assert.deepEqual(res.body, {
+				// 	issue_title: 'updated test',
+				// 	issue_text: 'updated test case',
+				// 	created_by: 'Phil',
+				// 	assigned_to: 'Joe',
+				// 	status_text: 'Not finished',
+				// 	created_at: new Date().toDateString(),
+				// 	updated_at: new Date().toDateString(),
+				// });
+				done();
+			});
+	});
+	test('Update an issue with missing _id: PUT request to /api/issues/{project}', function (done) {
+		chai
+			.request(server)
+			.put('/api/issues/apitest')
+			.send({
+				issue_title: 'updated test',
+			})
+			.end(function (err, res) {
+				assert.equal(
+					res.body.error,
+					'error updating issue, check the form input data types'
+				);
+				assert.equal(res.status, 400);
+				done();
+			});
+	});
 });
